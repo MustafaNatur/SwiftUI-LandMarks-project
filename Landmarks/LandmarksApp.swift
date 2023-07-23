3026a5c6ef744bf892b4bbda26e7173ac4745d2e
Mustafa Natur <mustafa.natur@mail.ru>
2023-07-23 23:18:48 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Мустафа Натур on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
